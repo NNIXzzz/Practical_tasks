ccf9b216ac2b594cbc20fe600c9b6fd85305dc0d
NNIXzzz <nik.iyunin@mail.ru>
2024-11-14 20:59:25 +0400

    Динамическая типизация

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+#Практическая работа по уроку "Динамическая типизация"
+
+name = 'Nikita'
+print (name)
+
+age = 22
+print (age)
+
+age = age + 1
+print (age)
+
+is_student = True
+print (is_student, type(is_student))
