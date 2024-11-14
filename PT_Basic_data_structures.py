643dcb47466511d4d1da0b17e0c7b912d4e9a3a1
NNIXzzz <nik.iyunin@mail.ru>
2024-11-14 20:19:00 +0400

    Базовые структуры данных

---
+++ PT_Basic_data_structures.py
@@ -0,0 +1,19 @@
+#Практическое задание по уроку "Базовые структуры данных"
+
+#1st program
+print((9**0.5)*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#4th program
+s = '123.456'
+f = float(s)*10
+i = int(f)
+i2 = i%10
+print(i2)
